b1ff3e9871c57e6ea3eeef1d03992e8617d645c3
Carlos <ccompean@life360.com>
2022-01-24 19:03:55 -0600

    debugging command

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/sirupsen/logrus v1.5.0
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/yaml.v2 v2.2.2
+	github.com/bmatcuk/doublestar/v2 v2.0.4
 )
--- project.go
+++ project.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"path"
 	"path/filepath"
-	"reflect"
 	"strings"
+
+	"github.com/bmatcuk/doublestar/v2"
 )
 
 func Min(x, y int) int {
@@ -54,19 +56,46 @@ func (p *Project) checkProjectRules(step map[interface{}]interface{}) bool {
 	return true
 }
 
+// matchPath checks if the file f matches the path p.
+// Taken from https://github.com/chronotc/monorepo-diff-buildkite-plugin/blob/602e650f10d54026fab466521c3202a04ae88afe/pipeline.go#L102
+func matchPath(p string, f string) (bool, error) {
+	// If the path contains a glob, the `doublestar.Match`
+	// method is used to determine the match,
+	// otherwise `strings.HasPrefix` is used.
+	if strings.Contains(p, "*") {
+		match, err := doublestar.Match(p, f)
+		if err != nil {
+			return false, fmt.Errorf("path matching failed: %v", err)
+		}
+		if match {
+			return true, nil
+		}
+	}
+	if strings.HasPrefix(f, p) {
+		return true, nil
+	}
+	return false, nil
+}
+
 func (p *Project) checkAffected(changedFiles []string) bool {
 	for _, filePath := range p.Path {
 		if filePath == "." {
 			return true
 		}
 		normalizedPath := path.Clean(filePath)
-		projectDirs := strings.Split(normalizedPath, "/")
 		for _, changedFile := range changedFiles {
-			changedDirs := strings.Split(changedFile, "/")
-			if reflect.DeepEqual(changedDirs[:Min(len(projectDirs), len(changedDirs))], projectDirs) {
+			if matched, _ := matchPath(normalizedPath, changedFile); matched {
 				return true
 			}
 		}
+
+		// projectDirs := strings.Split(normalizedPath, "/")
+		// for _, changedFile := range changedFiles {
+		// 	changedDirs := strings.Split(changedFile, "/")
+		// 	if reflect.DeepEqual(changedDirs[:Min(len(projectDirs), len(changedDirs))], projectDirs) {
+		// 		return true
+		// 	}
+		// }
 	}
 	return false
 }
